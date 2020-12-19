0e37d0b324f788bc209c2e145718041868ab4f55
Jman0519 <51171267+Jman0519@users.noreply.github.com>
2020-12-18 21:06:22 -0500

    Update profiler.js

--- profiler.js
+++ profiler.js
@@ -12,8 +12,13 @@ if(!global.Profiler){
 //function and all functions called from it to tell you what is chuggin your code.
 //IMPORTANT NOTE: The profiler is not guarenteed to work through a global code update, so
 //do not push your code while you are collecting data from it.
+//If looking at specific functions, make sure to name the functions. It will not pick up
+//on the property name of the function, only the function declaired name (no anonymous functions).
 function Profiler (){
-  
+
+  //If a global reset happens, I can compair this to memory to start/re-wrap so a global reset
+  //does not ruin a test.
+  this.running = false
   //Let the profiler know all of the things with functions that you want wrapped by default.
   //For instance, most people would want Creep and RoomPosition to see how expensive pathfinding
   //is. But very few people have anything in, or cares about anything in the Source prototype.
@@ -32,101 +37,103 @@ function Profiler (){
     //{ name: 'Memory', val: Memory },
     //{ name: 'RoomObject', val: RoomObject },
   ]
-  
+
   //this makes the profiler start fresh when you call it, and begin next tick
-  this.start = function(ticks, functionName){
+  this.start = function start(ticks, functionName){
     Memory.profiler = {}
     Memory.profiler.startTick = Game.time+1
     Memory.profiler.ticks = ticks
     Memory.profiler.functionName = functionName || null
+    Memory.profiler.running = true
+    Memory.profiler.shouldEmail = false
     return 'Profiler is set up and will begin next tick'
   }
-  
+
+  this.email = function email(ticks, functionName){
+    Memory.profiler = {}
+    Memory.profiler.startTick = Game.time+1
+    Memory.profiler.ticks = ticks
+    Memory.profiler.functionName = functionName || null
+    Memory.profiler.running = true
+    Memory.profiler.shouldEmail = true
+    return 'Profiler is set up and will begin next tick'
+  }
   //removeWrappers is called before adding wrappers every time so nothing gets wrapped twice (that would be bad for data)
   this.removeWrappers = function() {
     let startWrap = Game.cpu.getUsed()
     console.log('Removing Old Wrappers')
     var functionName = Memory.profiler.functionName
-    if(!functionName){//if there is no function name, profile all prototype functions
-      this.prototypes.forEach((proto) => {
-        let objectToWrap = proto.val.prototype || proto.val
-        if(!objectToWrap.profiler){
+    this.prototypes.forEach((proto) => {
+      let objectToWrap = proto.val.prototype || proto.val
+      if(!objectToWrap.profiler){
+        return
+      }
+      Object.getOwnPropertyNames(objectToWrap).forEach(property => {
+        const descriptor = Object.getOwnPropertyDescriptor(objectToWrap, property)
+        if(!descriptor){
           return
         }
-        Object.getOwnPropertyNames(objectToWrap).forEach(property => {
-          const descriptor = Object.getOwnPropertyDescriptor(objectToWrap, property)
-          if(!descriptor){
+
+        const hasAccessor = descriptor.get || descriptor.set//if it has custom accessors or mutators
+        if(hasAccessor){
+          //bootstrap the getters and setters?
+          if(!descriptor.configurable){
             return
           }
-          
-          const hasAccessor = descriptor.get || descriptor.set//if it has custom accessors or mutators
-          if(hasAccessor){
-            //bootstrap the getters and setters?
-            if(!descriptor.configurable){
-              return
-            }
-            
-            else{
-              return//profile the accessors and mutators
-            }
+
+          else{
+            return//profile the accessors and mutators
           }
-          
-          else if(typeof descriptor.value == "function"){
-            if(objectToWrap.profiler && objectToWrap.profiler.original != objectToWrap[property]){
-              objectToWrap[property] = objectToWrap.profiler.original
-              console.log(objectToWrap[property])
-            }
+        }
+
+        else if(typeof descriptor.value == "function"){
+          if(objectToWrap.profiler && objectToWrap.profiler[property] != objectToWrap[property]){
+            objectToWrap[property] = objectToWrap.profiler[property]
           }
-        })
+        }
       })
-    }
-    else{//else profile everything this functionName uses
-      if(console.log(arguments.callee.caller ? arguments.callee.caller.name : "global") == functionName){}//if the function was called from here
-      
-    }
+      objectToWrap.profiler = null
+    })
     console.log((Game.cpu.getUsed() - startWrap) + " CPU to remove wrappers.")
   }
-  
+
   //wrapFunctions makes every function store itself under this.prototype.oldFunc, replaces it, records data, and calls oldFunc
   this.wrapFunctions = function() {
     let startWrap = Game.cpu.getUsed()
     console.log('Adding New Wrappers')
     var functionName = Memory.profiler.functionName
-    if(!functionName){//if there is no function name, profile all prototype functions
-      this.prototypes.forEach((proto) => {
-        let objectToWrap = proto.val.prototype || proto.val
-        if(!objectToWrap.profiler){
-          objectToWrap.profiler = {}
-        }
-        else{
+    this.prototypes.forEach((proto) => {
+      let objectToWrap = proto.val.prototype || proto.val
+      objectToWrap.profiler = {}
+      Object.getOwnPropertyNames(objectToWrap).forEach(property => {
+        const descriptor = Object.getOwnPropertyDescriptor(objectToWrap, property)
+        if(!descriptor){
           return
         }
-        Object.getOwnPropertyNames(objectToWrap).forEach(property => {
-          
-          const descriptor = Object.getOwnPropertyDescriptor(objectToWrap, property)
-          if(!descriptor){
+
+        const hasAccessor = descriptor.get || descriptor.set//if it has custom accessors or mutators
+        if(hasAccessor){
+          if(!descriptor.configurable){
             return
           }
-          
-          const hasAccessor = descriptor.get || descriptor.set//if it has custom accessors or mutators
-          if(hasAccessor){
-            if(!descriptor.configurable){
-              return
-            }
-            
-            else{
-              return//profile the accessors and mutators
-            }
+
+          else{
+            return//profile the accessors and mutators
           }
-          
-          else if(typeof descriptor.value == "function"){
-            let oldFunc = objectToWrap[property]
-            var newFunc = function(){//make newFunc wrap oldFunc
-              
+        }
+
+        else if(typeof descriptor.value == "function"){
+          let oldFunc = objectToWrap[property]
+          objectToWrap[property] = function(){//make newFunc wrap oldFunc
+            let caller = ""
+            if(arguments.callee.caller){
+              caller = arguments.callee.caller.name
+            }
+            if(!functionName || functionName == caller || functionName == property){
               const start = Game.cpu.getUsed()
               const result = oldFunc.apply(this, arguments)
               const end = Game.cpu.getUsed()
-              
+
               if(!Memory.profiler[proto.name]){//record the data
                 Memory.profiler[proto.name] = {}
                 Memory.profiler[proto.name][property] = {}
@@ -134,29 +141,41 @@ function Profiler (){
               else if(!Memory.profiler[proto.name][property]){
                 Memory.profiler[proto.name][property] = {}
               }
-              
+
               Memory.profiler[proto.name][property].cpu += end-start
               Memory.profiler[proto.name][property].calls++
               return result//return the result of oldFunc
             }
-            
-            for(let prop in oldFunc){//incase a function has a property that needs copied
-              if(oldFunc.hasOwnProperty(prop)){
-                newFunc[prop] = oldFunc[prop]
-              }
+            else{
+              return oldFunc.apply(this, arguments)
             }
-            objectToWrap.profiler.original = oldFunc
-            objectToWrap[property] = newFunc
           }
-        })
+
+          for(let prop in oldFunc){//incase a function has a property that needs copied
+            if(oldFunc.hasOwnProperty(prop)){
+              objectToWrap[property][prop] = oldFunc[prop]
+            }
+          }
+          objectToWrap.profiler[property] = oldFunc
+        }
       })
-    }
-    
-    else{//else profile everything this functionName uses
-      if(console.log(arguments.callee.caller ? arguments.callee.caller.name : "global") == functionName){}//if the function was called from here
-      
-    }
+    })
+    //}
+
+    //else{//else profile everything this functionName uses
+    //  if(console.log(arguments.callee.caller ? arguments.callee.caller.name : "global") == functionName){}//if the function was called from here
+    //
+    //}
     console.log((Game.cpu.getUsed() - startWrap) + " CPU to wrap functions.")
+  }
+
+  this.displayMessage = function displayMessage(){
+    if(Memory.profiler.message){
+      return Memory.profiler.message
+    }
+    else{
+      return 'no message'
+    }
   }
   
   //do some basic calculations on the stored results
@@ -187,33 +206,46 @@ function Profiler (){
       if(sortedList[i] && sortedList[i].cpu && sortedList[i].calls && sortedList[i].name){
         lines[i+1] = []
         lines[i+1][0] = sortedList[i].name;
-        lines[i+1][1] = sortedList[i].cpu.toFixed(7);
+        lines[i+1][1] = sortedList[i].cpu.toPrecision(7);
         lines[i+1][2] = sortedList[i].calls;
-        
+
         firstLen = firstLen < sortedList[i].name.length ? sortedList[i].name.length : firstLen
       }
     }
     for(const lineIndex in lines){
       let line = lines[lineIndex]
-      while(line[0].length < firstLen+2){
+      while(line[0].length < firstLen){
         line[0] = line[0] + ` `
       }
     }
-    for(let i = 0; i < HOW_MANY && i < lines.length; i++){
-      console.log(lines[i].join('\t'))
+    Memory.profiler.proMessage = []
+    for(let i = 0; i < HOW_MANY+1 && i < lines.length; i++){
+      lines[i] = lines[i].join('\t')
+    }
+    let message = lines.join('\n')
+    Memory.profiler.proMessage = message
+    console.log(message)
+    if(Memory.profiler.shouldEmail == true){
+      Game.notify(message)
     }
   }
 }
 
 //Everything below here happens every tick and tells the profiler what to do (mostly based on Game.time and startTick)
+if(Memory.profiler && !Memory.profiler.running){
+  Memory.profiler.running = false
+}
 if(Memory.profiler && Memory.profiler.startTick && Memory.profiler.ticks){
-  if(Memory.profiler.startTick == Game.time){
-    //global.Profiler.removeWrappers()
-    global.Profiler.wrapFunctions()
+  if(Memory.profiler.startTick == Game.time || global.Profiler.running != Memory.profiler.running){
+    global.Profiler.removeWrappers()//remove any wrappers that might be stale (looking for the wrong caller)
+    global.Profiler.wrapFunctions()//wrap functions
+    global.Profiler.running = true
   }
   else if(Memory.profiler.startTick + Memory.profiler.ticks == Game.time){
-    global.Profiler.displayResults()
-    //global.Profiler.removeWrappers()//optional this should slightly improve code speed and force the profiler to stop recording
-    //Memory.profiler = null//should be fine, but errors do happen
+    global.Profiler.displayResults()//show the results, should be easy to strap to memory or an email
+    global.Profiler.removeWrappers()//optional this should slightly improve code speed and force the profiler to stop recording
+    let message = Memory.profiler.proMessage
+    Memory.profiler = {}//free up your memory
+    Memory.profiler.proMessgae = message//copys the results
   }
 }
